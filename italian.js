8f2fcf5e894266474d66e61d6c541913551a1796
Aspen Humphrey <aspenjadedimmick@gmail.com>
2017-06-15 09:54:45 -0500

    playing with italian function

--- italian.js
+++ italian.js
@@ -9,15 +9,18 @@ var translator = (function(language) {
 		}
 	];
 
-	console.log('italian', italian);
+	// console.log('italian', italian);
 // var sum = 0;
 // var obj = {prop1: 5, prop2: 13, prop3: 8};
 
-		language.printGreeting = (function() {
+		language.printItalian = function() {
+			let output = document.getElementById("translatedText");
+
 		for (var word in italian) {
-		  document.getElementById("translatedText").innerHTML += `${word.value} `;
+		  output.innerHTML += `<p>${italian[word].value}</p>`;
+		  console.log(italian);
 		};
-})
+};
 
 
 	// language.printItalian = function(){
